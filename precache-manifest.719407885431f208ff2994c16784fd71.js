ab49975581bde8b4c526344dae8b2e1ba604d1bc
DRE <andrew_ole@yahoo.com>
2020-01-18 15:32:04 +0100

    Updates

---
+++ precache-manifest.719407885431f208ff2994c16784fd71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e40c1ef4bb48fe300d623468d8559e7",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "c412bdd53cf216e9ca00",
+    "url": "/react-quiz/static/css/main.35908aa8.chunk.css"
+  },
+  {
+    "revision": "402f5ea6844ffdd6ead3",
+    "url": "/react-quiz/static/js/2.278dde14.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-quiz/static/js/2.278dde14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c412bdd53cf216e9ca00",
+    "url": "/react-quiz/static/js/main.a9a75ec2.chunk.js"
+  },
+  {
+    "revision": "781b9cc82827228dbc19",
+    "url": "/react-quiz/static/js/runtime-main.f9eff144.js"
+  }
+]);
